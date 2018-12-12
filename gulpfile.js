91387c4dee7f5b3994aa98e5a6e1e81133351a5b
Chris Barber <chris@cb1inc.com>
2018-12-12 11:02:49 -0600

    v1.1.2 chore: Updated deps. chore: Removed code climate. chore: Added appveyor config

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const manifest = require('./package.json');
 const path = require('path');
 const spawnSync = require('child_process').spawnSync;
 
+const { parallel, series } = gulp;
+
 const coverageDir = path.join(__dirname, 'coverage');
 const distDir = path.join(__dirname, 'dist');
 const docsDir = path.join(__dirname, 'docs');
@@ -14,18 +16,31 @@ const docsDir = path.join(__dirname, 'docs');
 /*
  * Clean tasks
  */
-gulp.task('clean', ['clean-coverage', 'clean-dist', 'clean-docs']);
+async function cleanCoverage() { return fs.remove(coverageDir); }
+async function cleanDist() { return fs.remove(distDir); }
+async function cleanDocs() { return fs.remove(docsDir); }
+exports.clean = parallel(cleanCoverage, cleanDist, cleanDocs);
 
-gulp.task('clean-coverage', done => { fs.remove(coverageDir, done); });
-
-gulp.task('clean-dist', done => { fs.remove(distDir, done); });
-
-gulp.task('clean-docs', done => { fs.remove(docsDir, done); });
+/*
+ * lint tasks
+ */
+async function lint(pattern) {
+	return gulp.src(pattern)
+		.pipe($.plumber())
+		.pipe($.eslint())
+		.pipe($.eslint.format())
+		.pipe($.eslint.failAfterError());
+}
+async function lintSrc() { return lint('src/**/*.js'); }
+async function lintTest() { return lint('test/**/test-*.js'); }
+exports['lint-src'] = lintSrc;
+exports['lint-test'] = lintTest;
+exports.lint = parallel(lintSrc, lintTest);
 
 /*
  * build tasks
  */
-gulp.task('build', ['clean-dist', 'lint-src'], () => {
+async function build() {
 	return gulp
 		.src('src/**/*.js')
 		.pipe($.plumber())
@@ -36,9 +51,11 @@ gulp.task('build', ['clean-dist', 'lint-src'], () => {
 		}))
 		.pipe($.sourcemaps.write())
 		.pipe(gulp.dest(distDir));
-});
+}
+exports.build = series(parallel(cleanDist, lintSrc), build);
+exports.default = exports.build;
 
-gulp.task('docs', ['lint-src', 'clean-docs'], () => {
+exports.docs = series(parallel(cleanDocs, lintSrc), async () => {
 	const esdoc = require('esdoc').default;
 
 	esdoc.generate({
@@ -68,29 +85,9 @@ gulp.task('docs', ['lint-src', 'clean-docs'], () => {
 });
 
 /*
- * lint tasks
- */
-function lint(pattern) {
-	return gulp.src(pattern)
-		.pipe($.plumber())
-		.pipe($.eslint())
-		.pipe($.eslint.format())
-		.pipe($.eslint.failAfterError());
-}
-
-gulp.task('lint-src', () => lint('src/**/*.js'));
-
-gulp.task('lint-test', () => lint('test/**/test-*.js'));
-
-/*
  * test tasks
  */
-gulp.task('test', ['build', 'lint-test', 'build'], () => runTests());
-gulp.task('test-only', ['lint-test', 'build'], () => runTests());
-gulp.task('coverage', ['clean-coverage', 'lint-src', 'lint-test', 'build'], () => runTests(true));
-gulp.task('coverage-only', ['clean-coverage', 'lint-test', 'build'], () => runTests(true));
-
-function runTests(cover) {
+async function runTests(cover) {
 	const args = [];
 	let { execPath } = process;
 
@@ -185,4 +182,7 @@ function resolveModule(name) {
 	}
 }
 
-gulp.task('default', ['build']);
+exports.test             = series(parallel(lintTest, build), () => runTests());
+exports['test-only']     = series(lintTest, () => runTests());
+exports.coverage         = series(parallel(cleanCoverage, lintTest, build), () => runTests(true));
+exports['coverage-only'] = series(parallel(cleanCoverage, lintTest), () => runTests(true));
--- test/setup.js
+++ test/setup.js
@@ -1,18 +1,4 @@
 import { expect } from 'chai';
 
 global.chai = require('chai');
-global.chai.use(require('sinon-chai'));
 global.expect = global.chai.expect;
-global.sinon = require('sinon');
-
-beforeEach(function () {
-	this.sandbox = global.sinon.createSandbox();
-	global.spy = this.sandbox.spy.bind(this.sandbox);
-	global.stub = this.sandbox.stub.bind(this.sandbox);
-});
-
-afterEach(function () {
-	delete global.spy;
-	delete global.stub;
-	this.sandbox.restore();
-});
